b159bd3c06e03ea6de6c1701448ddab3268d562f
aumoor02 <109328140+aumoor02@users.noreply.github.com>
2023-12-14 16:19:02 -0600

    merge

--- helloworld.py
+++ helloworld.py
@@ -1,21 +1,9 @@
-<<<<<<< HEAD
 def hello_world(city, state):
     print(f'Hello World {city}, {state}!')
 
 
 def main():
     hello_world('NYC', 'New York')
-
-
-main()
-=======
-def hello_world(city):
-    print(f'Hello World {city}!')
-
-
-def main():
-    hello_world('NYC')
->>>>>>> 4c72b7ba3d28446da079ff8812fe29cf65708c99
 
 
 main()
